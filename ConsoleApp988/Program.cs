73e5c9bfd7ff3d689872b09395fea312f16540b2
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-08-31 16:16:46 +0530

    Add project files.

---
+++ ConsoleApp988/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp988
+{
+    internal class Program
+    {
+        public class dog
+        {
+
+            public string breed { get; set; }
+            public string name { get; set; }
+
+            public void bark()
+            {
+                Console.WriteLine("bark ");
+            }
+        }
+        static void Main(string[] args)
+        {
+           dog bulldog = new dog();
+            bulldog.breed = "lab";
+            bulldog.name = "jimmy";
+            Console.WriteLine(bulldog.breed);
+            Console.WriteLine(bulldog.name);
+            bulldog.bark();
+            
+
+        }
+    }
+}
